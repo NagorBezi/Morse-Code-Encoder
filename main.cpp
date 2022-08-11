0ce76788d3635db212d922f92907f899b5a39e3e
Nagor Bezi <107315842+NagorBezi@users.noreply.github.com>
2022-08-11 15:52:49 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    while (true) {
+        char letter;
+
+        cout << "Enter alphabet or number to encode :";
+        cin >> letter;
+
+        if ((letter == 'a') || (letter == 'A')) {
+            cout << "Code : \n ._" << endl;
+        }
+        else if ((letter == 'b') || (letter == 'B')) {
+            cout << "Code : \n _..." << endl;
+        }
+        else if ((letter == 'c') || (letter == 'C')) {
+            cout << "Code : \n _._." << endl;
+        }
+        else if ((letter == 'd') || (letter == 'D')) {
+            cout << "Code : \n _.." << endl;
+        }
+        else if ((letter == 'e') || (letter == 'E')) {
+            cout << "Code : \n ." << endl;
+        }
+        else if ((letter == 'f') || (letter == 'F')) {
+            cout << "Code : \n .._." << endl;
+        }
+        else if ((letter == 'g') || (letter == 'G')) {
+            cout << "Code : \n __." << endl;
+        }
+        else if ((letter == 'h') || (letter == 'H')) {
+            cout << "Code : \n ...." << endl;
+        }
+        else if ((letter == 'i') || (letter == 'I')) {
+            cout << "Code : \n .." << endl;
+        }
+        else if ((letter == 'j') || (letter == 'J')) {
+            cout << "Code : \n .___" << endl;
+        }
+        else if ((letter == 'k') || (letter == 'K')) {
+            cout << "Code : \n _._" << endl;
+        }
+        else if ((letter == 'l') || (letter == 'L')) {
+            cout << "Code : \n ._.." << endl;
+        }
+        else if ((letter == 'm') || (letter == 'M')) {
+            cout << "Code : \n __" << endl;
+        }
+        else if ((letter == 'n') || (letter == 'N')) {
+            cout << "Code : \n _." << endl;
+        }
+        else if ((letter == 'o') || (letter == 'O')) {
+            cout << "Code : \n ___" << endl;
+        }
+        else if ((letter == 'p') || (letter == 'P')) {
+            cout << "Code : \n .__." << endl;
+        }
+        else if ((letter == 'q') || (letter == 'Q')) {
+            cout << "Code : \n __._" << endl;
+        }
+        else if ((letter == 'r') || (letter == 'R')) {
+            cout << "Code : \n ._." << endl;
+        }
+        else if ((letter == 's') || (letter == 'S')) {
+            cout << "Code : \n ..." << endl;
+        }
+        else if ((letter == 't') || (letter == 'T')) {
+            cout << "Code : \n _" << endl;
+        }
+        else if ((letter == 'u') || (letter == 'U')) {
+            cout << "Code : \n .._" << endl;
+        }
+        else if ((letter == 'v') || (letter == 'V')) {
+            cout << "Code : \n ..._" << endl;
+        }
+        else if ((letter == 'w') || (letter == 'W')) {
+            cout << "Code : \n .__" << endl;
+        }
+        else if ((letter == 'x') || (letter == 'X')) {
+            cout << "Code : \n _.._" << endl;
+        }
+        else if ((letter == 'y') || (letter == 'Y')) {
+            cout << "Code : \n _.__" << endl;
+        }
+        else if ((letter == 'z') || (letter == 'Z')) {
+            cout << "Code : \n __.." << endl;
+        }
+        else if (letter == '1') {
+            cout << "Code : \n .____" << endl;
+        }
+        else if (letter == '2') {
+            cout << "Code : \n ..___" << endl;
+        }
+        else if (letter == '3') {
+            cout << "Code : \n ...__" << endl;
+        }
+        else if (letter == '4') {
+            cout << "Code : \n ...._" << endl;
+        }
+        else if (letter == '5') {
+            cout << "Code : \n ....." << endl;
+        }
+        else if (letter == '6') {
+            cout << "Code : \n _...." << endl;
+        }
+        else if (letter == '7') {
+            cout << "Code : \n __..." << endl;
+        }
+        else if (letter == '8') {
+            cout << "Code : \n ___.." << endl;
+        }
+        else if (letter == '9') {
+            cout << "Code : \n ____." << endl;
+        }
+        else if (letter == '0') {
+            cout << "Code : \n _____" << endl;
+        }
+
+    }
+
+    return 0;
+}
